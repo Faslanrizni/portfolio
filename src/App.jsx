20c7b54a777d48f85d66e706e5599ef9fabc46f9
Faslan Rizni <Faslan2002Rizni@gmail.com>
2024-02-05 18:11:56 +0530

    navigation bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 
 
-const App = () => {
-  const [count, setCount] = useState(0)
-
+import { BrowserRouter } from "react-router-dom";
+import { About,Contact,Experience, Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas } from "./components";
+const App = () => {  
+  
   return (
-    <>
-      3D portfolio
-    </>
+    
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+              <Navbar/>
+              <Hero />
+            </div>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+        </div>
+
+      </BrowserRouter>
+  
   )
 }
 
