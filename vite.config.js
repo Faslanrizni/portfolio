2e80cfc9165d26d6523b6239c0009c0b33520c2b
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-10-19 17:41:52 +0530

    depoly error updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/3d-portfolio/',  // Add this line to set the base URL
 })
